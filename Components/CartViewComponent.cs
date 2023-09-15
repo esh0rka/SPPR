c3abbe29aab85ece84e08fc3f7e7fad261b94df7
esh0rka <brainett13@gmail.com>
2023-09-15 10:13:04 +0300

    Lab2

---
+++ Components/CartViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153504_Gaikevich
+{
+	public class CartViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WEB_153504_Gaikevich.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,20 @@ namespace WEB_153504_Gaikevich.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            ViewData["Message"] = "Лабораторная работа №2";
+
+            var items = new List<ListDemo>
+            {
+                new ListDemo { Id = 1, Name = "Item 1" },
+                new ListDemo { Id = 2, Name = "Item 2" },
+                new ListDemo { Id = 3, Name = "Item 3" },
+            };
+
+            var selectList = new SelectList(items, "Id", "Name");
+
+            return View(selectList);
         }
     }
 }
